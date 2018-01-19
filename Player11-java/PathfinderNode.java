85b80fa07ea9d69c5cb14646c2ef7f987bbeb6f2
xerobyte <psurrellr@gmail.com>
2018-01-19 13:36:39 -0500

    custom api v3

--- Player11-java/PathfinderNode.java
+++ Player11-java/PathfinderNode.java
@@ -1,7 +1,3 @@
-
-
-import bc.*;
-
 class PathfinderNode{
 
     int dir;
--- Player11-java/WorkerData.java
+++ Player11-java/WorkerData.java
@@ -1,12 +1,9 @@
-import bc.UnitType;
-
 public class WorkerData {
     int id;
     AuxMapLocation loc;
     int target_id;
     AuxMapLocation target_loc;
     int target_type;
-    //Direction safest_direction;
 
 
     WorkerData(int id){
@@ -14,6 +11,5 @@ public class WorkerData {
         target_id = -1;
         target_type = 0;
         target_loc = null;
-        //safest_direction = null;
     }
 }

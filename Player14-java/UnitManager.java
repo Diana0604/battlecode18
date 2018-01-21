978fb3d1d3784b6524c473cb782ea8e62a261b91
xerobyte <psurrellr@gmail.com>
2018-01-21 14:47:16 -0500

    minibugfix workers

--- Player14-java/UnitManager.java
+++ Player14-java/UnitManager.java
@@ -62,55 +62,61 @@ class UnitManager{
 
 
     static void initWorkerMaps(){
-        Data.blueprintsToBuild = new HashMap<>();
-        Data.blueprintsToPlace = new HashMap<>();
-        Data.structuresToRepair = new HashMap<>();
-        HashSet<AuxUnit> workers = new HashSet<>();
-        for (AuxUnit unit: Data.myUnits){
-            if (unit.getType() == UnitType.Worker) workers.add(unit);
-        }
-
-        for (int index: Data.structures){
-            //Per cada blueprint, crida els 8 workers mes propers a construir-lo
-            AuxUnit bp = Data.myUnits[index];
-            if (bp.isMaxHealth()) continue;
-            if (!bp.isBlueprint()) continue;
-
-            ArrayList<Pair> sorted = new ArrayList<>();
-            for (AuxUnit worker: workers){
-                Pair p = new Pair(bp.getMaplocation().distanceSquaredTo(worker.getMaplocation()), worker);
-                sorted.add(p);
+        try {
+            Data.blueprintsToBuild = new HashMap<>();
+            Data.blueprintsToPlace = new HashMap<>();
+            Data.structuresToRepair = new HashMap<>();
+            HashSet<AuxUnit> workers = new HashSet<>();
+            for (AuxUnit unit : Data.myUnits) {
+                if (unit.getType() == UnitType.Worker) workers.add(unit);
             }
-            sorted.sort((a, b) -> a.dist < b.dist ? -1 : a.dist == b.dist ? 0 : 1);
-            List<Pair> cut = sorted.subList(0,Math.min(8, sorted.size())); //no se si funciona si hi ha menys de 8 workers total
-            for (Pair p: cut){
-                int key = p.unit.getID();
-                int value = bp.getID();
-                if (!Data.blueprintsToBuild.containsKey(key)) Data.blueprintsToBuild.put(key, value);
-            }
-        }
 
-        for (int index: Data.structures){
-            //Per cada blueprint, crida els 8 workers mes propers a construir-lo
-            AuxUnit s = Data.myUnits[index];
-            if (s.isMaxHealth()) continue;
-            if (s.isBlueprint()) continue;
+            for (int index : Data.structures) {
+                //Per cada blueprint, crida els 8 workers mes propers a construir-lo
+                AuxUnit bp = Data.myUnits[index];
+                if (bp.isMaxHealth()) continue;
+                if (!bp.isBlueprint()) continue;
 
-            final int WORKERS_TO_CALL = 3;
-            ArrayList<Pair> sorted = new ArrayList<>();
-            for (AuxUnit worker: workers){
-                //no fiquem workers si ja son cridats per un blueprint
-                if (Data.blueprintsToBuild.containsKey(worker.getID())) continue;
-                Pair p = new Pair(s.getMaplocation().distanceSquaredTo(worker.getMaplocation()), worker);
-                sorted.add(p);
+                ArrayList<Pair> sorted = new ArrayList<>();
+                for (AuxUnit worker : workers) {
+                    AuxMapLocation workerloc = worker.getMaplocation();
+                    if (workerloc == null) continue;
+                    Pair p = new Pair(bp.getMaplocation().distanceSquaredTo(worker.getMaplocation()), worker);
+                    sorted.add(p);
+                }
+                sorted.sort((a, b) -> a.dist < b.dist ? -1 : a.dist == b.dist ? 0 : 1);
+                List<Pair> cut = sorted.subList(0, Math.min(8, sorted.size())); //no se si funciona si hi ha menys de 8 workers total
+                for (Pair p : cut) {
+                    int key = p.unit.getID();
+                    int value = bp.getID();
+                    if (!Data.blueprintsToBuild.containsKey(key)) Data.blueprintsToBuild.put(key, value);
+                }
             }
-            sorted.sort((a, b) -> a.dist < b.dist ? -1 : a.dist == b.dist ? 0 : 1);
-            List<Pair> cut = sorted.subList(0,Math.min(WORKERS_TO_CALL, sorted.size())); //no se si funciona si hi ha menys de 8 workers total
-            for (Pair p: cut){
-                int key = p.unit.getID();
-                int value = s.getID();
-                if (!Data.structuresToRepair.containsKey(key)) Data.structuresToRepair.put(key, value);
+
+            for (int index : Data.structures) {
+                //Per cada blueprint, crida els 8 workers mes propers a construir-lo
+                AuxUnit s = Data.myUnits[index];
+                if (s.isMaxHealth()) continue;
+                if (s.isBlueprint()) continue;
+
+                final int WORKERS_TO_CALL = 3;
+                ArrayList<Pair> sorted = new ArrayList<>();
+                for (AuxUnit worker : workers) {
+                    //no fiquem workers si ja son cridats per un blueprint
+                    if (Data.blueprintsToBuild.containsKey(worker.getID())) continue;
+                    Pair p = new Pair(s.getMaplocation().distanceSquaredTo(worker.getMaplocation()), worker);
+                    sorted.add(p);
+                }
+                sorted.sort((a, b) -> a.dist < b.dist ? -1 : a.dist == b.dist ? 0 : 1);
+                List<Pair> cut = sorted.subList(0, Math.min(WORKERS_TO_CALL, sorted.size())); //no se si funciona si hi ha menys de 8 workers total
+                for (Pair p : cut) {
+                    int key = p.unit.getID();
+                    int value = s.getID();
+                    if (!Data.structuresToRepair.containsKey(key)) Data.structuresToRepair.put(key, value);
+                }
             }
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 

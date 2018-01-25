1ec9c371d88d0c09a6a037196d35ccce60d4861f
dfolque99 <dfolque99@gmail.com>
2018-01-25 12:24:13 -0500

    Added communication and first messages mars->earth

--- Playermaster/GC.java
+++ Playermaster/GC.java
@@ -28,10 +28,19 @@ class GC {
             Research.initTurn();
             Units.initTurn(); //ha d'anar despres de Utils i Research
             Karbonite.initTurn(); //ha d'anar despres de Units
+            Communication.initTurn(); // ha d'anar abans que Explore
             Explore.initTurn(); //ha d'anar despres de Units
             Danger.initTurn(); //ha d'anar despres de Units
             WorkerUtil.initTurn(); //ha d'anar despres de Units i Karbonite
             Rocket.initTurn(); //ha d'anar despres de Units i Danger
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void endTurn() {
+        try {
+            Communication.endTurn();
         }catch(Exception e) {
             e.printStackTrace();
         }
